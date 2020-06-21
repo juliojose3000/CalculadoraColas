6efe5e1756700865abed5d05a342a5fa872a935d
Jeison Segura <jeisonsegura15@gmail.com>
2020-06-20 22:22:17 -0600

    MM1 listo

--- Controllers/MM1Controller.php
+++ Controllers/MM1Controller.php
@@ -1,5 +1,16 @@
 <?php
 
+		$Λ= $_POST['lambda'];
+		$M= $_POST['mu'];
+		$k= $_POST['k'];
 
+		$L=($Λ/($M-$Λ));
+		$W=(1*($M-$Λ));
+		$Lq=((pow($Λ,2))/($M*($M-$Λ)));
+		$Wq=($Λ/($M/($M-$Λ)));
 
+			echo "L=". $L. "\n";
+            echo "W= " .$W;
+            echo "Lq= ". $Lq;
+            echo "Wq= ". $Wq;
 ?>
