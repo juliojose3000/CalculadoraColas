498a0ce039b893ab81717e4c212874035d7df4a3
Julio <juliojose3000@gmail.com>
2020-06-20 22:27:29 -0600

    MD1 Controller

--- Controllers/MD1Controller.php
+++ Controllers/MD1Controller.php
@@ -1,4 +1,47 @@
 <?php
 
+    /*$µ = 12;
+    $λ = 8;
+
+    echo(longitudPromedioCola($µ,$λ));
+    echo("\n");
+    echo(tiempoEsperaPromedioCola($µ,$λ));
+    echo("\n");
+    echo(numeroPromedioClientesSistema($µ,$λ));
+    echo("\n");
+    echo(tiempoPromedioEnSistema($µ,$λ));*/
+
+    function longitudPromedioCola($µ, $λ){
+
+        $result = pow($λ, 2)/(2*$µ*($µ-$λ));
+
+        return $result;
+
+    }
+
+    function tiempoEsperaPromedioCola($µ, $λ){
+
+        $result = $λ/(2*$µ*($µ-$λ));
+
+        return $result;
+
+    }
+
+    function numeroPromedioClientesSistema($µ, $λ){
+
+        $result =  longitudPromedioCola($µ, $λ) + ($λ/$µ);
+
+        return $result;
+
+    }
+
+    function tiempoPromedioEnSistema($µ, $λ){
+
+        $result =  tiempoEsperaPromedioCola($µ, $λ) + ( 1 / $µ);
+
+        return $result;
+
+    }
+
 
 ?>
